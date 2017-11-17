5c27436561d2a4bfd301f2a33486d7ad71823446
Christian <cdemadrigal@hotmail.com>
2017-11-16 19:39:34 -0600

    se importarón paquetes esenciales

--- app/src/main/java/com/example/christian/sifacmovil/MainActivity.java
+++ app/src/main/java/com/example/christian/sifacmovil/MainActivity.java
@@ -2,6 +2,30 @@ package com.example.christian.sifacmovil;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.AsyncTask;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
